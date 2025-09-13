caf074a15c6ec152428da523d93ccebd9521e2b8
mateushmd <mateushmdiniz@gmail.com>
2025-09-13 21:25:19 +0000

    wsl configuration

--- configuration.nix
+++ configuration.nix
@@ -1,111 +1,7 @@
-{ self, config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, ... }:
+
 {
-  imports =
-  [ 
-    ./hardware-configuration.nix
-  ];
-
-  boot = {
-    supportedFilesystems = [ "ntfs" ];
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-  };
-
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-  };
-
-  time.timeZone = "America/Sao_Paulo";
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "pt_BR.UTF-8";
-      LC_IDENTIFICATION = "pt_BR.UTF-8";
-      LC_MEASUREMENT = "pt_BR.UTF-8";
-      LC_MONETARY = "pt_BR.UTF-8";
-      LC_NAME = "pt_BR.UTF-8";
-      LC_NUMERIC = "pt_BR.UTF-8";
-      LC_PAPER = "pt_BR.UTF-8";
-      LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "pt_BR.UTF-8";
-    };
-  };
-
-  services = {
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "br";
-        variant = "";
-      };
-    };
-    libinput.enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma6.enable = true;
-    printing.enable = true;
-    pulseaudio.enable = false;
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
-    };
-    openssh = {
-      enable = true;
-      ports = [ 22 ];
-      settings = {
-        PasswordAuthentication = true;
-        AllowUsers = null;
-        UseDns = true;
-        X11Forwarding = false;
-        PermitRootLogin = "no";
-      };
-    };
-  };
-
-  console.keyMap = "br-abnt2";
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  security.rtkit.enable = true;
-
-  users = {
-    users.mateus = {
-      isNormalUser = true;
-      description = "mateus";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-        kdePackages.kate
-      ];
-    };
-    defaultUserShell = pkgs.fish;
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
   programs = {
-    firefox.enable = true;
-    ssh = {
-      startAgent = true;
-      enableAskPassword = true;
-      askPassword = pkgs.lib.mkForce "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
-    };
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
-    };
     starship = {
       enable = true;
       settings = { 
@@ -118,37 +14,12 @@
 
   environment = {
     localBinInPath = true;
-    sessionVariables.SSH_ASKPASS_REQUIRE = "prefer";
     systemPackages = with pkgs; [
-      wezterm
-      kitty
-
-      vim 
-      inputs.nixvim.packages.${pkgs.system}.default 
+      vim
+      inputs.nixvim.packages.${pkgs.system}.default
       git
-      zip
-      unzip    
-      fzf
+      unzip
       bat
-      gitmoji-cli
-      
-      kdePackages.ksshaskpass
-      gparted
-
-      eza
-      neofetch
-      btop
-
-      discord
-      obsidian
-      anydesk
-      kdePackages.kcalc
-      floorp
-      vlc
-
-      jdk
-      iverilog
-
       nerd-fonts.jetbrains-mono
     ];
   };
@@ -165,6 +36,4 @@
     };
   };
 
-  # Just don't 
-  system.stateVersion = "24.11"; # Did you read the comment?
 }
--- flake.nix
+++ flake.nix
@@ -1,17 +1,26 @@
 {
-  description = "Was it really worth it?";
+  description = "This should not exist";
 
   inputs = {
+    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:mateushmd/nixvim";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
       modules = [
-        ./configuration.nix 
+        nixos-wsl.nixosModules.default 
+        {
+          imports = [ ./configuration.nix ];
+
+          wsl.enable = true;
+          wsl.defaultUser = "mateus";
+          system.stateVersion = "25.05"; 
+        }
       ];
     };
   };
