e75e497c0b7c1e69eb7873e622c91196a8acba56
mateushmd <mateushmdiniz@gmail.com>
2026-08-12 23:52:19 -0300

    config: no more dotfiles!

--- dotfiles/.bashrc
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-alias fzf='fzf --preview="bat --color=always {}"'
-alias ff='cd $(find * -type d | fzf)'
-alias vzf='vi $(fzf)'
-
-alias rebuild-flake="sudo nixos-rebuild switch --flake '/home/mateus/nixos-flake'"
-alias ls="eza --icons --group-directories-first"
--- user/fish/default.nix
+++ user/fish/default.nix
@@ -1,4 +1,27 @@
 { ... }: 
 {
-  programs.fish.enable = true; 
+  programs.fish = {
+    enable = true; 
+    interactiveShellInit = ''
+      if status is-interactive
+        set -g fish_greeting ""
+
+        alias rebuild-system="sudo nixos-rebuild switch --flake /home/mateus/repos/nixos#phalanx"
+        alias check-system="sudo nixos-rebuild dry-activate --flake /home/mateus/repos/nixos#phalanx"
+        alias test-system="sudo nixos-rebuild test --flake /home/mateus/repos/nixos#phalanx"
+        alias ls="eza --icons --group-directories-first"
+        alias lt="ls --tree"
+
+        alias nix-shell="nix-shell --run fish"
+
+        alias fzf='fzf --preview="bat --color=always {}"'
+        alias ff='cd (find . -type d | fzf)'
+        alias vzf='nvim (fzf)'
+
+        alias icat="kitty +kitten icat"
+
+        alias gemini="agy"
+      end
+    '';
+  };
 }
