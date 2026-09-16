ba793f44464b485c8b021408d4c04411b49a541b
mateushmd <mateushmdiniz@gmail.com>
2026-09-16 11:17:22 +0200

    hyprland: keyboard layout switch finally working

--- wrapped/hyprland/phalanx.lua
+++ wrapped/hyprland/phalanx.lua
@@ -4,3 +4,16 @@ hl.monitor({
     position = "0x0",
     scale = 1
 })
+
+hl.device({
+    name = "at-translated-set-2-keyboard",
+    kb_layout = "br",
+    kb_variant = "abnt2"
+})
+
+hl.device({
+    name = "by-tech-gaming-keyboard",
+    kb_layout = "us",
+    kb_variant = "intl"
+})
+
